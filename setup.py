bf2df5b4d1e883ca9949a60354f4abf0c0cd6878
SAM IRELAND <sam.ireland.uk@gmail.com>
2015-08-02 21:11:40 +0100

    1.0.0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name="pychats",
-      version="1.0.0",
+      version="1.0.1",
       description="Conversation analytics",
       long_description="Tools for accessing conversation backups from various sources, and analysing said conversations.",
       url="https://github.com/samirelanduk/pychats",
