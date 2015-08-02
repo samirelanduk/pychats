b5348699acfa77ea7ae62b94268efc0d40a5949a
SAM IRELAND <sam.ireland.uk@gmail.com>
2015-08-02 21:04:47 +0100

    1.0.0

--- pychats/setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup
-
-setup(name="pychats",
-      version='1.0',
-      description="Conversation analytics",
-      author="Sam Ireland",
-      author_email="sam.ireland.uk@gmail.com",
-      url="samireland.com",
-      packages=["pychats"])
