9651ac39e04fba80d52223642f9ffd7d6508f51e
Sam Ireland <sam.ireland.uk@gmail.com>
2016-11-19 22:50:16 +0000

    Add restrictions on Message addition to Conversations

--- pychats/chats/conversations.py
+++ pychats/chats/conversations.py
@@ -1,3 +1,5 @@
+from .messages import Message
+
 class Conversation:
 
     def __init__(self):
@@ -9,4 +11,14 @@ class Conversation:
 
 
     def add_message(self, message):
+        if not isinstance(message, Message):
+            raise TypeError(
+             "Can only add Message objects with, not '%s'" % str(message)
+            )
+        if message in self._messages:
+            raise ValueError(
+             "Cannot add %s ('%s') to %s as it is already present" % (
+              str(message), message.text(), self
+             )
+            )
         self._messages.append(message)
--- tests/conversation_tests/test_conversations.py
+++ tests/conversation_tests/test_conversations.py
@@ -28,3 +28,14 @@ class ConversationMessagesTests(TestCase):
         self.assertEqual(self.conversation._messages, [self.messages[0]])
         self.conversation.add_message(self.messages[1])
         self.assertEqual(self.conversation._messages, self.messages[0:2])
+
+
+    def test_can_only_add_messages_to_conversations(self):
+        with self.assertRaises(TypeError):
+            self.conversation.add_message("Some message")
+
+
+    def test_cannot_add_message_if_it_is_already_present(self):
+        self.conversation.add_message(self.messages[0])
+        with self.assertRaises(ValueError):
+            self.conversation.add_message(self.messages[0])
