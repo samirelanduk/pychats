ac64c055eaa61b040d88aa792cdf70da23ce0d74
Sam Ireland <sam.ireland.uk@gmail.com>
2016-11-19 22:32:23 +0000

    Add ability to add messages

--- pychats/chats/conversations.py
+++ pychats/chats/conversations.py
@@ -6,3 +6,7 @@ class Conversation:
 
     def __repr__(self):
         return "<Conversation (%i messages)>" % len(self._messages)
+
+
+    def add_message(self, message):
+        self._messages.append(message)
--- tests/conversation_tests/test_conversations.py
+++ tests/conversation_tests/test_conversations.py
@@ -1,5 +1,6 @@
 from unittest import TestCase
-from pychats.chats import Conversation
+from unittest.mock import Mock
+from pychats.chats import Conversation, Contact, Message
 
 class ConversationCreationTests(TestCase):
 
@@ -11,3 +12,19 @@ class ConversationCreationTests(TestCase):
     def test_conversation_repr(self):
         conversation = Conversation()
         self.assertEqual(str(conversation), "<Conversation (0 messages)>")
+
+
+
+class ConversationMessagesTests(TestCase):
+
+    def setUp(self):
+        self.senders = [Mock(Contact) for _ in range(3)]
+        self.messages = [Mock(Message) for _ in range(3)]
+        self.conversation = Conversation()
+
+
+    def test_can_add_messages_to_conversation(self):
+        self.conversation.add_message(self.messages[0])
+        self.assertEqual(self.conversation._messages, [self.messages[0]])
+        self.conversation.add_message(self.messages[1])
+        self.assertEqual(self.conversation._messages, self.messages[0:2])
