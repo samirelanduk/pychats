10f3ebea20804ce9aa00ae9a793ea20487bf7d97
Sam Ireland <sam.ireland.uk@gmail.com>
2016-11-19 21:30:40 +0000

    Add Conversation repr

--- pychats/chats/conversations.py
+++ pychats/chats/conversations.py
@@ -2,3 +2,7 @@ class Conversation:
 
     def __init__(self):
         self._messages = []
+
+
+    def __repr__(self):
+        return "<Conversation (%i messages)>" % len(self._messages)
--- tests/conversation_tests/test_conversations.py
+++ tests/conversation_tests/test_conversations.py
@@ -6,3 +6,8 @@ class ConversationCreationTests(TestCase):
     def test_can_create_conversation(self):
         conversation = Conversation()
         self.assertEqual(conversation._messages, [])
+
+
+    def test_conversation_repr(self):
+        conversation = Conversation()
+        self.assertEqual(str(conversation), "<Conversation (0 messages)>")
