a219be0f78fb3850b0263ebdeffe94422a8e92d0
Sam Ireland <sam.ireland.uk@gmail.com>
2017-04-14 21:10:15 +0100

    Refactor contact unit tests

--- tests/conversation_tests/test_contacts.py
+++ tests/conversation_tests/test_contacts.py
@@ -13,15 +13,18 @@ class ContactCreationTests(TestCase):
             Contact(111)
 
 
+
+class ContactReprTests(TestCase):
+
     def test_contacts_repr(self):
         contact = Contact("Marvin Goodwright")
         self.assertEqual(str(contact), "<Contact: Marvin Goodwright>")
 
 
 
-class ContactPropertiesTests(TestCase):
+class ContactNameTests(TestCase):
 
-    def test_contact_properties(self):
+    def test_contact_name_property(self):
         contact = Contact("Marvin Goodwright")
         self.assertIs(contact._name, contact.name())
 
@@ -29,7 +32,7 @@ class ContactPropertiesTests(TestCase):
     def test_can_modify_contact_properties(self):
         contact = Contact("Marvin Goodwright")
         contact.name("Albus Dumbledore")
-        self.assertEqual(contact.name(), "Albus Dumbledore")
+        self.assertEqual(contact._name, "Albus Dumbledore")
 
 
     def test_can_only_set_contact_name_to_str(self):
