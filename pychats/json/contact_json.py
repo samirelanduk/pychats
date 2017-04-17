6d7b9c33404f6b62ced2746419b4155cef77871f
Sam Ireland <sam.ireland.uk@gmail.com>
2017-04-17 20:31:22 +0100

    Add JSON to Contact function

--- pychats/json/contact_json.py
+++ pychats/json/contact_json.py
@@ -3,7 +3,7 @@
 from ..chats.people import Contact
 
 def contact_to_json(contact):
-    """Takes a :py:class:`Contact` and converts it to a JSON dict.
+    """Takes a :py:class:`.Contact` and converts it to a JSON dict.
 
     :param Contact contact: the contact object to convert.
     :rtype: ``dict``"""
@@ -11,3 +11,16 @@ def contact_to_json(contact):
     if not isinstance(contact, Contact):
         raise TypeError("'%s' is not a Contact object" % str(contact))
     return {"name": contact.name()}
+
+
+def json_to_contact(json):
+    """Creates a py:class:`.Contact` from a JSON ``dict``.
+
+    :param dict json: The ``dict`` to convert.
+    :rtype: ``Contact``"""
+    
+    if not isinstance(json, dict):
+        raise TypeError("'%s' is not a dict" % str(json))
+    if "name" not in json:
+        raise ValueError("Contact json must have 'name' key: %s" % str(json))
+    return Contact(json["name"])
--- tests/json_tests/test_contact_json.py
+++ tests/json_tests/test_contact_json.py
@@ -1,11 +1,11 @@
 from unittest import TestCase
 from unittest.mock import Mock
-from pychats.json.contact_json import contact_to_json
+from pychats.json.contact_json import contact_to_json, json_to_contact
 from pychats.chats.people import Contact
 
-class ContactToJsontests(TestCase):
+class ContactToJsonTests(TestCase):
 
-    def test_can_make_contact_from_json(self):
+    def test_can_make_json_from_contact(self):
         contact = Mock(Contact)
         contact.name.return_value = "Lord Asriel"
         json = contact_to_json(contact)
@@ -15,3 +15,23 @@ class ContactToJsontests(TestCase):
     def test_contact_to_json_requires_contact(self):
         with self.assertRaises(TypeError):
             contact_to_json("some string")
+
+
+
+class JsonToContactTests(TestCase):
+
+    def test_can_make_contact_from_json(self):
+        json = {"name": "Lord Asriel"}
+        contact = json_to_contact(json)
+        self.assertIsInstance(contact, Contact)
+        self.assertEqual(contact._name, "Lord Asriel")
+
+
+    def test_contact_from_json_requires_dict(self):
+        with self.assertRaises(TypeError):
+            json_to_contact("some string")
+
+
+    def test_contact_from_json_requires_name_key(self):
+        with self.assertRaises(ValueError):
+            json_to_contact({"wrongkey": "Lord Asriel"})
