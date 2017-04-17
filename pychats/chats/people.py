273957b98ca40b84fca20c1015d95ec4b9537d00
Sam Ireland <sam.ireland.uk@gmail.com>
2017-04-17 20:35:10 +0100

    Add docstrings for people.py

--- pychats/chats/people.py
+++ pychats/chats/people.py
@@ -1,4 +1,9 @@
+"""This module contains the Contact class used to represent people."""
+
 class Contact:
+    """A person who has sent at least one message.
+
+    :param str name: The person's name."""
 
     def __init__(self, name):
         if not isinstance(name, str):
@@ -11,6 +16,11 @@ class Contact:
 
 
     def name(self, name=None):
+        """Returns the contact's name. If a string is provided, the name will be
+        updated to that.
+
+        :param str name: If given, the contact's name will be updated."""
+        
         if name:
             if not isinstance(name, str):
                 raise TypeError("name must be str, not '%s'" % name)
