e8955a5999e6e118e162444c9538aafd85daa1af
Sam Ireland <sam.ireland.uk@gmail.com>
2017-05-15 19:39:24 +0100

    Update Contact JSON docstrings

--- pychats/json/contact_json.py
+++ pychats/json/contact_json.py
@@ -6,6 +6,7 @@ def contact_to_json(contact):
     """Takes a :py:class:`.Contact` and converts it to a JSON dict.
 
     :param Contact contact: the contact object to convert.
+    :raises TypeError: if something other than a py:class:`.Contact` is given.
     :rtype: ``dict``"""
 
     if not isinstance(contact, Contact):
@@ -20,6 +21,9 @@ def json_to_contact(json):
     """Creates a py:class:`.Contact` from a JSON ``dict``.
 
     :param dict json: The ``dict`` to convert.
+    :raises TypeError: if something other than a ``dict`` is given.
+    :raises ValueError: if the ``dict`` doesn't have a ``name`` key.
+    :raises ValueError: if the ``dict`` doesn't have a ``tags`` key.
     :rtype: ``Contact``"""
 
     if not isinstance(json, dict):
