7cc0bb983c63468d970629bfdd2010350155afbd
Sam Ireland <sam.ireland.uk@gmail.com>
2017-05-31 00:06:33 +0100

    Add JSON to Message conversion function

--- pychats/json/message_json.py
+++ pychats/json/message_json.py
@@ -1,7 +1,9 @@
 """This module handles the conversion of Message objects to JSON and back."""
 
+from datetime import datetime
 from ..chats.messages import Message
-from .contact_json import contact_to_json
+from ..chats.people import Contact
+from .contact_json import contact_to_json, json_to_contact
 
 def message_to_json(message):
     """Takes a :py:class:`.Message` and converts it to a JSON dict.
@@ -17,3 +19,46 @@ def message_to_json(message):
      "timestamp": message.timestamp().strftime("%Y-%m-%d %H:%M:%S"),
      "sender": contact_to_json(message.sender())
     }
+
+
+def json_to_message(json, contacts):
+    """Creates a py:class:`.Message` from a JSON ``dict``.
+
+    You must also supply an iterable of zero or more py:class:`.Contact`
+    objects - if the name of sender in the JSON matches one of these people,
+    that object will be set as the sender. Otherwise a new py:class:`.Contact`
+    will be created. Otherwise a new py:class:`.Contact` would be created for
+    each message.
+
+    :param dict json: The ``dict`` to convert.
+    :param contacts: An iterable of py:class:`.Contact` objects.
+    :raises TypeError: if something other than a ``dict`` is given.
+    :raises ValueError: if the ``dict`` doesn't have a ``text`` key.
+    :raises ValueError: if the ``dict`` doesn't have a ``timestamp`` key.
+    :raises ValueError: if the ``dict`` doesn't have a ``sender`` key.
+    :raises TypeError: if none py:class:`.Contact` objects are given.
+    :rtype: ``Message``"""
+    
+    if not isinstance(json, dict):
+        raise TypeError("'%s' is not a dict" % str(json))
+    if "text" not in json:
+        raise ValueError("Message json must have 'text' key: %s" % str(json))
+    if "timestamp" not in json:
+        raise ValueError("Message json must have 'timestamp' key: %s" % str(json))
+    if "sender" not in json:
+        raise ValueError("Message json must have 'sender' key: %s" % str(json))
+    for contact in contacts:
+        if not isinstance(contact, Contact):
+            raise TypeError("'%s' is not a Contact" % str(contact))
+    sender = None
+    for person in contacts:
+        if person.name() == json["sender"]["name"]:
+            sender = person
+            break
+    else:
+        sender = json_to_contact(json["sender"])
+    return Message(
+     json["text"],
+     datetime.strptime(json["timestamp"], "%Y-%m-%d %H:%M:%S"),
+     sender
+    )
--- tests/json_tests/test_message_json.py
+++ tests/json_tests/test_message_json.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from unittest import TestCase
 from unittest.mock import Mock, patch
-from pychats.json.message_json import message_to_json
+from pychats.json.message_json import message_to_json, json_to_message
 from pychats.chats.messages import Message
 from pychats.chats.people import Contact
 
@@ -28,3 +28,67 @@ class MessageToJsonTests(TestCase):
     def test_message_to_json_requires_message(self):
         with self.assertRaises(TypeError):
             message_to_json("some string")
+
+
+
+class JsonToMessageTests(TestCase):
+
+    def test_can_make_message_with_existing_contact(self):
+        contact1 = Mock(Contact)
+        contact1.name.return_value = "Justin Powers"
+        contact2 = Mock(Contact)
+        contact2.name.return_value = "Lydia Powers"
+        json = {
+         "text": "message text",
+         "timestamp": "2009-05-23 12:12:01",
+         "sender": {"name": "Justin Powers", "tags": ["tag1", "tag2"]}
+        }
+        message = json_to_message(json, [contact1, contact2])
+        self.assertEqual(message._text, "message text")
+        self.assertEqual(message._timestamp, datetime(2009, 5, 23, 12, 12, 1))
+        self.assertIs(message._sender, contact1)
+
+
+    @patch("pychats.json.message_json.json_to_contact")
+    def test_can_make_message_with_new_contact(self, mock_contact):
+        contact1 = Mock(Contact)
+        contact1.name.return_value = "Justin Powers"
+        contact2 = Mock(Contact)
+        contact2.name.return_value = "Lydia Powers"
+        contact3 = Mock(Contact)
+        mock_contact.return_value = contact3
+        json = {
+         "text": "message text",
+         "timestamp": "2009-05-23 12:12:01",
+         "sender": {"name": "Marvin Powers", "tags": ["tag1", "tag2"]}
+        }
+        message = json_to_message(json, [contact1, contact2])
+        self.assertEqual(message._text, "message text")
+        self.assertEqual(message._timestamp, datetime(2009, 5, 23, 12, 12, 1))
+        self.assertIs(message._sender, contact3)
+        mock_contact.assert_called_with(json["sender"])
+
+
+    def test_json_to_message_requires_dict(self):
+        with self.assertRaises(TypeError):
+            json_to_message("some string")
+
+
+    def test_json_to_message_requires_text_key(self):
+        with self.assertRaises(ValueError):
+            json_to_message({"wrong": "txt", "timestamp": "", "sender": ""}, [])
+
+
+    def test_json_to_message_requires_timestamp_key(self):
+        with self.assertRaises(ValueError):
+            json_to_message({"text": "txt", "wrong": "", "sender": ""}, [])
+
+
+    def test_json_to_message_requires_sender_key(self):
+        with self.assertRaises(ValueError):
+            json_to_message({"text": "txt", "timestamp": "", "wrong": ""}, [])
+
+
+    def test_json_to_message_needs_contacts(self):
+        with self.assertRaises(TypeError):
+            json_to_message({"text": "", "timestamp": "", "sender": ""}, ["s"])
