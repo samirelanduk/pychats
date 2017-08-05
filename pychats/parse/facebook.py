14bf693a22a5d7c464388a8cccf46320c80e05cc
Sam Ireland <sam.ireland.uk@gmail.com>
2017-08-05 22:48:36 +0100

    Add function for getting JSON from Faceboom thread

--- pychats/parse/facebook.py
+++ pychats/parse/facebook.py
@@ -2,15 +2,45 @@
 messages.htm files."""
 
 from bs4 import BeautifulSoup
+from bs4.element import Tag
+from datetime import datetime
 
 def html_to_threads(html):
     """Takes the html string of a file and gets the thread divs from it as
     ``BeautifulSoup`` Tag objects.
 
-    :param str html: The HTML string."""
+    :param str html: The HTML string.
+    :raises TypeError: if non-string HTML is given.
+    :returns: ``list`` of ``Tag`` objects"""
 
     if not isinstance(html, str):
         raise TypeError("HTML must be provided as a string")
     soup = BeautifulSoup(html, "html.parser")
     threads = soup.find_all("div", {"class" : "thread"})
     return threads
+
+
+def thread_to_json(thread):
+    """Takes a Thread div and turns it into the JSON of a
+    pychats :py:class:`.Conversation` object.
+
+    :param Tag thread: The ``BeautifulSoup`` Tag of a Facebook thread.
+    :rtype: ``list``"""
+
+    if not isinstance(thread, Tag):
+        raise TypeError("Need a BeautifulSoup tag, not {}".format(thread))
+    messages = thread.find_all("div", {"class" : "message"})
+    paragraphs = thread.find_all("p")
+    messages = zip(messages, paragraphs)
+    json = []
+    for message, paragraph in messages:
+        name = message.find_all("span", {"class" : "user"})[0].text.strip()
+        meta = message.find_all("span", {"class" : "meta"})[0].text.strip()
+        date_text = " ".join(meta.split(" ")[:-1])
+        date = datetime.strptime(date_text, "%A, %d %B %Y at %H:%M")
+        json.append({
+         "text": paragraph.text,
+         "sender": {"tags": [], "name": name},
+         "timestamp": date.strftime("%Y-%m-%d %H:%M:%S")
+        })
+    return json
--- tests/parse_tests/test_facebook.py
+++ tests/parse_tests/test_facebook.py
@@ -1,6 +1,7 @@
 from unittest import TestCase
 from unittest.mock import Mock, patch
 import pychats.parse.facebook as fb
+from bs4 import BeautifulSoup
 from bs4.element import Tag
 
 class HtmlToThreadsTests(TestCase):
@@ -21,3 +22,32 @@ class HtmlToThreadsTests(TestCase):
     def test_html_to_thread_requires_string(self):
         with self.assertRaises(TypeError):
             fb.html_to_threads(100)
+
+
+
+class ThreadToJSONTests(TestCase):
+
+    def test_can_convert_thread_to_json(self):
+        thread = BeautifulSoup("""<div class="thread">90@fbok.com, 100@fbok.com
+        <div class="message"><div class="message_header">
+        <span class="user">John Ronn</span>
+        <span class="meta">Friday, 19 August 2016 at 13:13 UTC+01</span>
+        </div></div><p>Hello?</p><div class="message">
+        <div class="message_header"><span class="user">Myke</span>
+        <span class="meta">Friday, 19 August 2016 at 13:15 UTC+01</span>
+        </div></div><p>Hello!</p></div></div>""", "html.parser").find("div")
+        json = fb.thread_to_json(thread)
+        self.assertEqual(json, [{
+         "text": "Hello?",
+         "sender": {"name": "John Ronn", "tags": []},
+         "timestamp": "2016-08-19 13:13:00"
+        }, {
+         "text": "Hello!",
+         "sender": {"name": "Myke", "tags": []},
+         "timestamp": "2016-08-19 13:15:00"
+        }])
+
+
+    def test_html_to_thread_requires_thread(self):
+        with self.assertRaises(TypeError):
+            fb.thread_to_json("<html>")
