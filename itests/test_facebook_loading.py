76ccdd27e80179839a802a2c60e8933ce8c321ec
Sam Ireland <sam.ireland.uk@gmail.com>
2017-08-05 21:57:33 +0100

    Add 2.2 itest

---
+++ itests/test_facebook_loading.py
@@ -0,0 +1,31 @@
+from datetime import datetime
+import os
+from unittest import TestCase
+import pychats
+
+class Tests(TestCase):
+
+    def test_load_facebook_messages(self):
+        log = pychats.from_facebook("itests/test_files/messages.htm")
+
+        self.assertEqual(log.name(), "Facebook")
+        convs = sorted(log.conversations(), key=lambda k: len(k.messages()))
+        self.assertEqual(len(convs), 4)
+        convs.reverse()
+        conv1, conv2, conv3, conv4 = convs
+
+        self.assertEqual(len(conv1.messages()), 6)
+        self.assertEqual(conv1.messages()[0].text(), "Hello!")
+        self.assertEqual(
+         conv1.messages()[0].timestamp(), datetime(2014, 8, 28, 0, 19, 0)
+        )
+        self.assertEqual(conv1.messages()[0].sender().name(), "John Ronn")
+        self.assertEqual(conv1.messages()[-1].text(), "Never mind!")
+
+        self.assertEqual(len(conv2.messages()), 5)
+        self.assertEqual(conv1.messages()[0].text(), "Hi all.")
+        self.assertEqual(
+         conv1.messages()[0].timestamp(), datetime(2017, 6, 8, 12, 31, 0)
+        )
+        self.assertEqual(conv1.messages()[0].sender().name(), "444@facebook.com")
+        self.assertEqual(conv1.messages()[-1].text(), "All things pass into eternity.")
